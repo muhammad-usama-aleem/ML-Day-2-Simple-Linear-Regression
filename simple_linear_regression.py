4027071277e6dd4761fcc64622f3cf89fdbc5f3f
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2021-08-04 18:30:32 +0500

    till data prediction

---
+++ simple_linear_regression.py
@@ -0,0 +1,28 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
+
+
+# fitting simple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# predicting the test results
+y_predict = regressor.predict(x_test)
+print(y_test)
+print(y_predict)
